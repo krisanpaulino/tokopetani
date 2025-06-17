5eaebb9811a905dd1eb23e110c09fdcc40f4df2d
krisanpaulino <36087959+krisanpaulino@users.noreply.github.com>
2025-06-17 12:05:19 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -127,7 +127,7 @@ class HomeController extends Controller
                 'origin' => '34462',
                 'destination' => $destination,
                 'weight' => 1000,
-                'courier' => 'jne:jnt:sicepat:anteraja'
+                'courier' => 'jne'
             ),
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 30,
